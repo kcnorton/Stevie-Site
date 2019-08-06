dbbfb2fa88edf93d402c2d3864a6b8794bc2301b
KARA <nortonkara057@gmail.com>
2019-08-05 21:53:13 -0400

    Changed fonts

--- react-client/src/components/headerComponent/navBar.js
+++ react-client/src/components/headerComponent/navBar.js
@@ -5,21 +5,21 @@ class NavBar extends Component {
   render() {
     return (
       <header>
-        
-        <div className="logo">
-          STEVIE
-        </div>
+        <div className="header">
+          <div className="logo">
+            STEVIE
+          </div>
 
-        <nav>
-          <ul id="headerButtons">
-            <li className="navButton"><Link to="/ContactPage"><span>Contact</span></Link></li>
-            <li className="navButton"><Link to="/TimelinePage"><span>Timeline</span></Link></li>
-            <li className="navButton"><Link to="/AboutPage"><span>About</span></Link></li>
-            <li className="navButton"><Link to="/HomePage"><span>Home</span></Link></li>
+          <nav>
+            <ul id="headerButtons">
+              <li className="navButton"><Link to="/ContactPage"><span>Contact</span></Link></li>
+              <li className="navButton"><Link to="/TimelinePage"><span>Timeline</span></Link></li>
+              <li className="navButton"><Link to="/AboutPage"><span>About</span></Link></li>
+              <li className="navButton"><Link to="/HomePage"><span>Home</span></Link></li>
             
-          </ul>
-        </nav>
-      
+            </ul>
+          </nav>
+        </div>
       </header>
     );
   }
