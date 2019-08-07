46e6618b1016167db3a564e37c0f6db6d87abb09
KARA <nortonkara057@gmail.com>
2019-08-07 19:09:05 -0400

    Added Fade to Timeline effects

--- react-client/src/components/pages/timelinePage.js
+++ react-client/src/components/pages/timelinePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Fade from 'react-reveal/Fade';
 import Stairs from '../../images/stairs.jpg';
 import CentralPark from '../../images/centralpark.jpg';
 import Walter from '../../images/stevienwalter.jpg';
@@ -36,9 +37,9 @@ class TimelinePage extends Component {
 
 // listen for events
 componentDidMount() {
-    window.addEventListener("load", callbackFunc)
-    window.addEventListener("resize", callbackFunc)
-    window.addEventListener("scroll", callbackFunc)
+    window.addEventListener("load", callbackFunc())
+    window.addEventListener("resize", callbackFunc())
+    window.addEventListener("scroll", callbackFunc())
 }
   render() {
     return (
@@ -49,40 +50,44 @@ componentDidMount() {
 
           <ul>
             <li>
-              <div className="timelineImage">
-                <img src={Puppy} alt="puppy stevie"/>
-                <time>2012</time> Stevie was born on August 19th and spent the first couple of months in Sarasota, FL with her nine siblings.
-              </div>
+              <Fade right>
+                <div className="timelineImage">
+                  <img src={Puppy} alt="puppy stevie"/>
+                  <time>2012</time> Stevie was born on August 19th and spent the first couple of months in Sarasota, FL with her nine siblings.
+                </div>
+              </Fade>
             </li>
             <li>
-              <div className="timelineImage">
-                <img src={Walter} alt="stevie and walter"/>
-                <time>2012</time> Stevie moved to Gainesville, FL and met her best friend Walter.
-              </div>
+              <Fade left>
+                <div className="timelineImage">
+                  <img src={Walter} alt="stevie and walter"/>
+                  <time>2012</time> Stevie moved to Gainesville, FL and met her best friend Walter.
+                </div>
+              </Fade>
             </li>
             <li>
-              <div className="timelineImage">
-                <img src={Stairs} alt="stevie sitting on stairs"/>
-                <time>2014</time> Stevie moved to Chattanooga, TN where she enjoyed hiking and climbing in nearby forests and parks.
-              </div>
+              <Fade right>
+                <div className="timelineImage">
+                  <img src={Stairs} alt="stevie sitting on stairs"/>
+                  <time>2014</time> Stevie moved to Chattanooga, TN where she enjoyed hiking and climbing in nearby forests and parks.
+                </div>
+              </Fade>
             </li>
             <li>
-              <div className="timelineImage">
-                <img src={Snowstorm} alt="stevie running in a snowstorm"/>
-                <time>2015</time> Stevie moved to Baltimore, MD and played in her first snowstorm.
-              </div>
+              <Fade left>
+                <div className="timelineImage">
+                  <img src={Snowstorm} alt="stevie running in a snowstorm"/>
+                  <time>2015</time> Stevie moved to Baltimore, MD and played in her first snowstorm.
+                </div>
+              </Fade>
             </li>
             <li>
-              <div className="timelineImage">
-                <img src={CentralPark} alt="stevie glowing in central park"/>
-                <time>2017</time> Stevie moved to Brooklyn, NY and learned that she loves city life and all the people who pet her.
-              </div>
-            </li>
-            <li>
-              <div className="timelineImage">
-                <img src={Stairs} alt="stevie sitting on stairs"/>
-                <time>2019</time> Stevie established her first boulder
-              </div>
+              <Fade right>
+                <div className="timelineImage">
+                  <img src={CentralPark} alt="stevie glowing in central park"/>
+                  <time>2017</time> Stevie moved to Brooklyn, NY and learned that she loves city life and all the people who pet her.
+                </div>
+              </Fade>
             </li>
           </ul>
       	</div>
